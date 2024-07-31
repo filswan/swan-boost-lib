662f203584fedb490938d0c429d78fa72836b7d0
sonic <longzhang>
2024-07-31 18:11:14 +0800

    update check boost health

--- provider/provider.go
+++ provider/provider.go
@@ -10,6 +10,7 @@ import (
 	"github.com/filswan/swan-boost-lib/client"
 	myask "github.com/filswan/swan-boost-lib/storedask"
 	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"net/http"
 	"strconv"
 	"time"
@@ -121,8 +122,8 @@ func (pc *Client) MarketSetAsk(ctx context.Context, boostRepo string, fullNodeUr
 	return storedAsk.SetAsk(ctx, chain_type.BigInt(pri), chain_type.BigInt(vpri), abi.ChainEpoch(qty), miner, opts...)
 }
 
-func (pc *Client) CheckBoostStatus(ctx context.Context) error {
-	return pc.stub.BoostIndexerAnnounceAllDeals(ctx)
+func (pc *Client) CheckBoostStatus(ctx context.Context) (peer.ID, error) {
+	return pc.stub.ID(ctx)
 }
 
 func (pc *Client) BoostDirectDeal(ctx context.Context, boostRepo string, fullNodeUrl string, walletAddress string, allocationId string, filepath string, piececidStr string, isDelete bool) (*DealRejectionInfo, error) {
